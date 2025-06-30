8c33689753cf097d8cf997a40de67028264ec0bd
Eduardo Sandoval <edsandoval.dev@gmail.com>
2025-06-29 18:43:44 -0600

    Eliminando API inestable

--- apps/API-promo-configurator/Models/Dtos/EstadoDto.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace API_promo_configurator.Models.Dtos;
-
-public class EstadoDto
-{
-    public int IdEstado { get; set; }
-    public string Nombre { get; set; } = null!;
-} 
