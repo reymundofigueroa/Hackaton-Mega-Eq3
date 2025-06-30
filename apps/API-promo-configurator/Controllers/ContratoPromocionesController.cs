975a97ed1a92cc001020d93f28f7041e3a640153
Eduardo Sandoval <edsandoval.dev@gmail.com>
2025-06-30 16:44:23 -0600

    Endpoint para asigar una promoción a un contrato listo

--- apps/API-promo-configurator/Controllers/ContratoPromocionesController.cs
+++ apps/API-promo-configurator/Controllers/ContratoPromocionesController.cs
@@ -1,3 +1,4 @@
+using API_promo_configurator.Models;
 using API_promo_configurator.Models.Dtos;
 using API_promo_configurator.Repository.IRepository;
 using AutoMapper;
@@ -29,7 +30,7 @@ namespace API_promo_configurator.Controllers
             return Ok(contratoPromocionesDto);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetContratoPromocion")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetContratoPromocion(int id)
@@ -47,6 +48,33 @@ namespace API_promo_configurator.Controllers
             var promociones = _contratoPromocionRepository.GetPromocionesPorContrato(idContrato);
             var promocionesDto = promociones.Select(cp => _mapper.Map<ContratoPromocionDto>(cp)).ToList();
             return Ok(promocionesDto);
+        }
+
+        // Endpoint para asignar una promoción a un contrato
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)] // El usuario no está autorizado a acceder a este recurso
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // Realizó mala petición
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)] // Realizó mala petición
+        [ProducesResponseType(StatusCodes.Status201Created)] // Se creó correctamente
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)] // Error en el servidor
+        public IActionResult AsignarPromoContrato([FromBody] CreateContratoPromocionDto createContratoPromocionDto)
+        {
+            if (createContratoPromocionDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // TODO: Verificar que el contrato no tenga ya esa promoción asignada
+
+            var promocionContrato = _mapper.Map<ContratoPromocione>(createContratoPromocionDto);
+
+            if (!_contratoPromocionRepository.CreateContratoPromocion(promocionContrato))
+            {
+                ModelState.AddModelError("CustomError", $"Algo salió mal al asignar la promoción al contrato con el ID {promocionContrato.IdContrato}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetContratoPromocion", new { id = promocionContrato.IdContratoPromocion }, promocionContrato);
         }
     }
 } 
